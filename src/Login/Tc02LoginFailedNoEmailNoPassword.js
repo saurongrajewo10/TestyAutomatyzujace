41439b7e05fcf75fcd0142a7f5dcb5af5d49deed
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2020-02-17 19:18:55 +0100

    TC04LoginFailedNoEmailNoPassword

---
+++ src/Login/Tc02LoginFailedNoEmailNoPassword.js
@@ -0,0 +1,11 @@
+const { Builder, By, Key, until, WebDriver } = require("selenium-webdriver")
+var webdriver = require('selenium-webdriver');
+
+async function Tc02LoginFailedNoEmailNoPassword() {
+    console.clear();
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("http://automationpractice.com/index.php")
+    await driver.findElement(By.xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')).click();
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="SubmitLogin"]'))).click();
+}
+Tc02LoginFailedNoEmailNoPassword()
