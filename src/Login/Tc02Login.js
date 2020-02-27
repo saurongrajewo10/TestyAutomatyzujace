469a1840906880c1b91b8f548ed63ff1a34f41e4
danieldawid <daniel.dawid@contractors.roche.com>
2020-02-27 08:13:01 +0100

    ZebranieTCLoginWcalosc

--- src/Login/Tc02Login.js
+++ src/Login/Tc02Login.js
@@ -1,9 +1,22 @@
+const { Tc02LoginFailedInvalidEmailFormat } = require("./Tc02LoginFailedInvalidEmailFormat");
+const { Tc02LoginFailedNoEmail } = require("./Tc02LoginFailedNoEmail");
 const { Tc02LoginFailedNoEmailNoPassword } = require("./Tc02LoginFailedNoEmailNoPassword");
+const { Tc02LoginFailedWrongPassword } = require("./Tc02LoginFailedWrongPassword");
+const { Tc02LoginSuccess } = require("./Tc02LoginSuccess");
+
 const { Builder } = require("selenium-webdriver");
 
 async function Tc02Login() {
+    try {
     let driver = await new Builder().forBrowser("chrome").build();
-    Tc02LoginFailedNoEmailNoPassword(driver);
+    await Tc02LoginFailedInvalidEmailFormat(driver);
+    await Tc02LoginFailedNoEmail(driver);
+    await Tc02LoginFailedNoEmailNoPassword(driver);
+    await Tc02LoginFailedWrongPassword(driver);
+    await Tc02LoginSuccess(driver);
+}
+catch {
 }
 
+}
 module.exports.Tc02Login = Tc02Login;
--- src/Login/Tc02LoginFailedWrongPassword.js
+++ src/Login/Tc02LoginFailedWrongPassword.js
@@ -1,14 +1,15 @@
 const { Builder, By, Key, until } = require("selenium-webdriver")
 
-async function Tc02LoginFailedNoEmailNoPassword(driver) {
+async function Tc02LoginFailedWrongPassword(driver) {
     try {
         await driver.get("http://automationpractice.com/index.php");
         await driver.findElement(By.xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')).click();
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="SubmitLogin"]'))).click();
+        await driver.wait(until.elementLocated(By.xpath('//*[@id="email"]'))).sendKeys("bobtest@gmail.com");
+        await driver.wait(until.elementLocated(By.xpath('//*[@id="passwd"]'))).sendKeys("ZleHaslo", Key.RETURN);
         await driver.wait(until.elementLocated(By.xpath('//*[@id="center_column"]/div[1]')));
 
         var testStatement = await driver.findElement(By.xpath('//*[@id="center_column"]/div[1]/ol')).getText();
-        if (testStatement.match(/An email address required./)) {
+        if (testStatement.match(/Authentication failed./)) {
         }
         else {
             console.log("Inappropriate error statement has been displayed");
@@ -17,9 +18,8 @@ async function Tc02LoginFailedNoEmailNoPassword(driver) {
         return true;
     }
     catch {
-        console.log('Error Tc02LoginFailedNoEmailNoPassword');
+        console.log('Error Tc02LoginFailedWrongPassword');
         return false;
     }
 }
-
-module.exports.Tc02LoginFailedNoEmailNoPassword = Tc02LoginFailedNoEmailNoPassword;
+module.exports.Tc02LoginFailedWrongPassword = Tc02LoginFailedWrongPassword;
