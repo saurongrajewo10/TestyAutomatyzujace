801a4d3037531d42526c0dc4ea38c8b1bf6fac9e
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2020-02-27 19:50:10 +0100

    tc04 add to cart, go to checkout

--- src/ShoppingProcess/Tc04ShoppingProcessRemovingProductFromCart.js
+++ src/ShoppingProcess/Tc04ShoppingProcessRemovingProductFromCart.js
@@ -1,19 +1,21 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const webdriver = require("selenium-webdriver"),
+    By = webdriver.By,
+    until = webdriver.until;
 
 async function Tc04ShoppingProcessRemovingProductFromCart() {
-    //try {
-    let driver = await new Builder().forBrowser("chrome").build();
-    driver.manage().window().maximize();
-    await driver.get("http://automationpractice.com/index.php");
-    const actions = driver.actions({ bridge: true });
-    var elem = await driver.findElement(By.xpath('//*[@id="homefeatured"]/li[1]/div/div[1]/div/a[1]/img'));
-    await actions.move({ duration: 1000, origin: elem, x: 0, y: 0 }).perform();
-    await driver.wait(until.elementLocated(By.xpath('//*[@id="homefeatured"]/li[1]/div/div[2]/div[2]/a[1]/span'))).click();
-    //await driver.wait(until.elementLocated(By.xpath('/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span/i'))).click();
+    try {
+        let driver = await new webdriver.Builder().forBrowser("chrome").build();
+        driver.manage().window().maximize();
+        await driver.get("http://automationpractice.com/index.php");
+        const actions = driver.actions({ bridge: true });
+        var elem = await driver.findElement(By.xpath('//*[@id="homefeatured"]/li[1]/div/div[1]/div/a[1]/img'));
+        await actions.move({ duration: 1000, origin: elem, x: 0, y: 0 }).perform();
+        await driver.wait(until.elementLocated(By.xpath('//*[@id="homefeatured"]/li[1]/div/div[2]/div[2]/a[1]/span'))).click();
+        await driver.manage().setTimeouts({ implicit: 2000 }).then(() => { driver.findElement(By.xpath('//*[@id="layer_cart"]/div[1]/div[2]/div[4]/a')).click() });
 
-    // }
-    // catch {
-    //     console.log('Error Tc04ShoppingProcessRemovingProductFromCart');
-    // }
+    }
+    catch {
+        console.log('Error Tc04ShoppingProcessRemovingProductFromCart');
+    }
 }
 Tc04ShoppingProcessRemovingProductFromCart();
