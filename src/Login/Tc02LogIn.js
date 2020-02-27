f8ddc44777581eb599f55daa64bd425c973bf702
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2020-02-27 17:24:49 +0100

    fixing

--- src/Login/Tc02LogIn.js
+++
@@ -1,9 +0,0 @@
-const { Tc02LoginFailedNoEmailNoPassword } = require("./Tc02LoginFailedNoEmailNoPassword");
-const { Builder } = require("selenium-webdriver");
-
-async function Tc02Login() {
-    let driver = await new Builder().forBrowser("chrome").build();
-    Tc02LoginFailedNoEmailNoPassword(driver);
-}
-
-module.exports.Tc02Login = Tc02Login;
